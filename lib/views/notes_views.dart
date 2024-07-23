7ab00013ad2bcd69afe03f9b4afa29bcf897beb1
kareemsalah720@gmail.com <kareemsalah720@gmail.com>
2024-07-23 05:28:51 +0300

    create body with Appbar

--- lib/views/notes_views.dart
+++ lib/views/notes_views.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
-
+import 'package:notes/widgets/notes_app_body.dart';
 class NotesViews extends StatelessWidget {
   const NotesViews({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: CustomBody(),
+    );
   }
 }
---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:notes/widgets/custom_search_icon.dart';
+
+class CustomAppbar extends StatelessWidget {
+  const CustomAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      
+      children: [      
+        Text(
+          'Notes',
+          style: TextStyle(
+            fontSize: 28,
+          ),
+        ),
+        Spacer(),
+        CustomSearchIcon(),
+      ],
+    );
+  }
+}
+
