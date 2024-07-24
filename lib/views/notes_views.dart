b1b4bef5e741f44544029991031821aeeb8e1dcb
kareemsalah720@gmail.com <kareemsalah720@gmail.com>
2024-07-24 03:19:01 +0300

    create showModalBottomSheet

--- lib/views/notes_views.dart
+++ lib/views/notes_views.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:notes/widgets/AddNote_Bottom_Sheet.dart';
 import 'package:notes/widgets/notes_app_body.dart';
+
 class NotesViews extends StatelessWidget {
   const NotesViews({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       floatingActionButton: FloatingActionButton(
-        backgroundColor:const Color.fromARGB(255, 61, 55, 55),
-        onPressed: (){},
-        child:const Icon(Icons.add) ,
-      
+        backgroundColor: const Color.fromARGB(255, 61, 55, 55),
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return const AddNoteBottomSheet();
+              });
+        },
+        child: const Icon(Icons.add),
       ),
-      body:const CustomBody(),
+      body: const CustomBody(),
     );
   }
 }
